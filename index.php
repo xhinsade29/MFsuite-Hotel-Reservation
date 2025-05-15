7b3daf5c448805519964f0cb02c5ecb0bc734729
xhinsade29 <lopezkristine749@gmail.com>
2025-05-15 14:22:50 +0800

    added the style

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
   <title>MFsuite Hotel - Home</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"/>
-  <link rel="stylesheet" href="styles/home.css">
+  <link rel="stylesheet" href="./styles/home.css">
 
- 
 </head>
 <body>
 
